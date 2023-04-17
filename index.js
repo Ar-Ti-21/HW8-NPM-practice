bca80d63eea31f12e2a54197eabe80ed2aca96df
Ar-Ti-21 <120409609+Ar-Ti-21@users.noreply.github.com>
2023-04-17 17:06:56 +0300

    Add files via upload
    add files to repository for HW-8

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function findAdd (a, b) {
+    const add = a + b;
+    return add;
+}
+
+function findSub (a, b) {
+    const sub = a + b;
+    return sub;
+}
+
+function findMult (a, b) {
+    const mult = a + b;
+    return mult;
+}
+
+
